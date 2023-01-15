2a514ed7ccbc34de06261a29a54489f750049e37
fawait626 <fawait626gmail.com>
2023-01-15 16:09:05 +0700

    update terbaru

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -4,7 +4,7 @@ import { CallSilent, CloseSquare, TickSquare } from "react-iconly";
 
 const httpRequest = axios.create({
   // baseURL: process.env.REACT_APP_BASE_URL,
-  baseURL: "https://api.sinduadihebat.my.id/",
+  baseURL: "https://backend.sinduadihebat.my.id/",
 });
 
 httpRequest.interceptors.request.use(
